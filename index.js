ef7b3ebfd85fa7003c5ba1f43d99be2565ad19a6
dhiraj-sinha <59096898+dhiraj-sinha@users.noreply.github.com>
2022-03-04 18:59:22 +0530

    Delete index.js
    Not in use by dhiraj

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/",(req,res)=> {
-    res.send("Node on AWS");
-});
-app.listen("3000",()=>{
-    console.log("Server is running at 3000 port")
-})
